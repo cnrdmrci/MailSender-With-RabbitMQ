bda180d1516c1ee641bf2e3c2e65d15851cfe1e3
cnrdmrci <canerdemirci.mail@gmail.com>
2020-02-28 23:35:34 +0300

    mail gönderen ana sınıf oluşturuldu ve bağımlılıkları tanımlandı.

---
+++ MailServer/MailServerProject/Concrete/MailGonderen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MailServerProject.Abstract;
+
+namespace MailServerProject.Concrete
+{
+    public class MailGonderen
+    {
+        private IMailIslemcisi _mailIslemcisi;
+        private ISmtpAyarlayan _smtpAyarlayan;
+        private IVeritabaniIslemcisi _veritabaniIslemcisi;
+        private IMailKontrolcu _mailKontrolcu;
+        public MailGonderen(IMailIslemcisi mailIslemcisi, ISmtpAyarlayan smtpAyarlayan, IVeritabaniIslemcisi veritabaniIslemcisi, IMailKontrolcu mailKontrolcu)
+        {
+            _mailIslemcisi = mailIslemcisi;
+            _smtpAyarlayan = smtpAyarlayan;
+            _veritabaniIslemcisi = veritabaniIslemcisi;
+            _mailKontrolcu = mailKontrolcu;
+        }
+    }
+}
