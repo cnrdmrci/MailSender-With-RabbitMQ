966c26a8c338b0c7619c756beb160033c379c2e7
cnrdmrci <canerdemirci.mail@gmail.com>
2020-02-29 17:23:19 +0300

    nesneden direk gönderim sağlayan motor tanımlandı.

--- MailServer/MailServerProject/Concrete/MailMotoru.cs
+++ MailServer/MailServerProject/Concrete/MailMotoru.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MailServerProject.Model;
 
 namespace MailServerProject.Concrete
 {
     public class MailMotoru
     {
-
+        public MailGonderimSonuc MailGonder(MailBilgi mailgBilgi)
+        {
+            MailGonderen mailGonderen = new MailGonderen(new MailIslemcisi(), new SmtpAyarlayan(), new VeritabaniIletisimcisi(), new MailKontrolcu());
+            return mailGonderen.Gonder(mailgBilgi);
+        }
     }
 }
