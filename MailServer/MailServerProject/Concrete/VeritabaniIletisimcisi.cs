0c4b2a61741a7bede0f1faf0a4a6f9dc6e1520aa
cnrdmrci <canerdemirci.mail@gmail.com>
2020-02-29 17:21:25 +0300

    veritabani iletisimci somut sinif eklendi.

---
+++ MailServer/MailServerProject/Concrete/VeritabaniIletisimcisi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MailServerProject.Abstract;
+using MailServerProject.Model;
+
+namespace MailServerProject.Concrete
+{
+    public class VeritabaniIletisimcisi : IVeritabaniIslemcisi
+    {
+        public void BasariliGonderimKaydet(MailBilgi mailBilgi)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void HataliGonderimKaydet(MailBilgi mailBilgi, string mesaj)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
