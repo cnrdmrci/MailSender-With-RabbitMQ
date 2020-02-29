21506596dfb4a1f65d630e2923cd2ea8aaf93b44
cnrdmrci <canerdemirci.mail@gmail.com>
2020-02-29 12:37:56 +0300

    smtp ayarlari icin fonksiyon tanimlandi.

--- MailServer/MailServerProject/Abstract/ISmtpAyarlayan.cs
+++ MailServer/MailServerProject/Abstract/ISmtpAyarlayan.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MailServerProject.Model;
 
 namespace MailServerProject.Abstract
 {
     public interface ISmtpAyarlayan
     {
+        SmtpAyar SmtpAyarGetir();
     }
 }
