624dbc387abb48edd89f9308812cf10c5a72b653
cnrdmrci <canerdemirci.mail@gmail.com>
2020-02-29 14:10:34 +0300

    yeni özellikler eklendi.

--- MailServer/MailServerProject/Model/MailBilgi.cs
+++ MailServer/MailServerProject/Model/MailBilgi.cs
@@ -7,11 +7,13 @@ namespace MailServerProject.Model
 {
     public class MailBilgi
     {
-        public string Kime { get; set; }
         public string Kimden { get; set; }
         public string KimdenUnvan { get; set; }
+        public List<string> GonderilecekEpostaAdresleri { get; set; }
+        public List<string> CcEpostaAdresleri { get; set; }     //Bilgilendirilecek e-posta adresleri
+        public List<string> BccEpostaAdresleri { get; set; }    //Gizli olarak eklenecek e-posta adresleri
         public string Konu { get; set; }
         public string Icerik { get; set; }
-        public List<string> EkDosya { get; set; }
+        public List<string> EklenecekDosyaAdresleri { get; set; }
     }
 }
