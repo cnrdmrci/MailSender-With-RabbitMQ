ae473f9518c8b04129d232eaeec3f5f9f468dd0c
cnrdmrci <canerdemirci.mail@gmail.com>
2020-02-29 17:23:00 +0300

    hataları işleyebilmemiz için özellik eklendi.

--- MailServer/MailServerProject/Model/MailGonderimSonuc.cs
+++ MailServer/MailServerProject/Model/MailGonderimSonuc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MailServerProject.Model
@@ -7,5 +8,12 @@ namespace MailServerProject.Model
     public class MailGonderimSonuc
     {
         public bool Basarili { get; set; }
+        public List<string> Hatalar { get; set; }
+
+        public string HatalarStr
+        {
+            get { return Hatalar.Aggregate((x,y)=>x+"\r\n"+y); }
+            private set { }
+        }
     }
 }
