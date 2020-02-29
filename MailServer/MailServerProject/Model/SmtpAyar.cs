f2544a6353a261e951d1c4e3c079354619b4afaf
cnrdmrci <canerdemirci.mail@gmail.com>
2020-02-29 13:58:27 +0300

    port string veritipinden int veri tipine cevirildi.

--- MailServer/MailServerProject/Model/SmtpAyar.cs
+++ MailServer/MailServerProject/Model/SmtpAyar.cs
@@ -7,7 +7,7 @@ namespace MailServerProject.Model
     public class SmtpAyar
     {
         public string Host { get; set; }
-        public string Port { get; set; }
+        public int Port { get; set; }
         public string KullaniciAdi { get; set; }
         public string Sifre { get; set; }
         public bool SslKullan { get; set; }
