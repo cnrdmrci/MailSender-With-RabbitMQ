28c9a8ffa785187dd110d8b39e8bef164f8dd643
cnrdmrci <canerdemirci.mail@gmail.com>
2020-02-29 17:21:56 +0300

    mail gönderen sınıfı tamamlandı. gönderime hazır

--- MailServer/MailServerProject/Concrete/MailGonderen.cs
+++ MailServer/MailServerProject/Concrete/MailGonderen.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using MailServerProject.Abstract;
+using MailServerProject.Model;
 
 namespace MailServerProject.Concrete
 {
@@ -17,6 +19,48 @@ namespace MailServerProject.Concrete
             _smtpAyarlayan = smtpAyarlayan;
             _veritabaniIslemcisi = veritabaniIslemcisi;
             _mailKontrolcu = mailKontrolcu;
+        }
+
+        public MailGonderimSonuc Gonder(MailBilgi mailBilgi)
+        {
+            MailGonderimSonuc mailGonderimSonuc = new MailGonderimSonuc();
+            mailGonderimSonuc.Hatalar = new List<string>();
+            try
+            {
+                //Bilgi kontrol et hataliysa logla ve basarisiz don.
+                mailGonderimSonuc = bilgiKontroluYap(mailBilgi, mailGonderimSonuc);
+                if (!mailGonderimSonuc.Basarili)
+                    return mailGonderimSonuc;
+
+                //mail gonderimi yap. exception alinmazsa basarilidir.
+                _mailIslemcisi.MailGonderimiYap(mailBilgi,_smtpAyarlayan);
+
+                //Gönderim başarılı
+                mailGonderimSonuc.Basarili = true;
+                _veritabaniIslemcisi.BasariliGonderimKaydet(mailBilgi);
+                return mailGonderimSonuc;
+
+            }
+            catch (Exception ex)
+            {
+                mailGonderimSonuc.Hatalar.Add(ex.Message);
+                _veritabaniIslemcisi.HataliGonderimKaydet(mailBilgi,mailGonderimSonuc.HatalarStr);
+                mailGonderimSonuc.Basarili = false;
+                return mailGonderimSonuc;
+            }
+
+        }
+
+        private MailGonderimSonuc bilgiKontroluYap(MailBilgi mailBilgi,MailGonderimSonuc mailGonderimSonuc)
+        {
+            if (!_mailKontrolcu.MailAdresiKontrolEt(mailBilgi))
+            {
+                mailGonderimSonuc.Hatalar.Add("Mail adresi hatalı, lütfen kontrol edin.");
+                _veritabaniIslemcisi.HataliGonderimKaydet(mailBilgi, "Mail adresi hatalı, lütfen kontrol edin.");
+                mailGonderimSonuc.Basarili = false;
+            }
+
+            return mailGonderimSonuc;
         }
     }
 }
